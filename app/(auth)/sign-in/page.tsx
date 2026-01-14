b0b616deac54ba7bb6f59b1ada5b2ad3794d8337
RBlaylock-Dev <rblaylock.prsvr@gmail.com>
2026-01-14 16:46:23 -0600

    started and finished the login page and sign up page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const SignIn = () => <AuthForm type="sign-in" />
+//   {
+//   return (
+//     <div>Sign In</div>
+//   )
+// }
+
+export default SignIn
