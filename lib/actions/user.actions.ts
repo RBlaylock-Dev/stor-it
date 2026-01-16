2a65ab25b7f6cfc8852a29f90edd9f3261a2b459
RBlaylock-Dev <rblaylock.prsvr@gmail.com>
2026-01-16 12:41:54 -0600

    Finialize the authentication system adding signInUser and signOutUser functions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
 import { avatarPlaceholderUrl } from "@/constants";
+import { redirect } from "next/navigation";
 
 // ** Create account flow**
 // 1. User enters full name and email
@@ -99,4 +100,33 @@ export const getCurrentUser = async () => {
     if(user.total <= 0) return null;
 
     return parseStringify(user.documents[0]);
+};
+
+export const signOutUser = async () => {
+    const { account } = await createSessionClient();
+    
+    try {
+        await account.deleteSession('current');
+        (await cookies()).delete("appwrite-session");
+        console.log('User logged out successfully');
+    } catch (error) {
+        handleError(error, "Failed to sign out user");
+    } finally {
+        redirect("/sign-in");
+    }
+};
+
+export const signInUser = async ({ email }: { email: string }) => {
+    try {
+        const existingUser = await getUserByEmail(email);
+
+        if(existingUser) {
+            await sendEmailOTP({ email });
+            return parseStringify({ accountId: existingUser.accountId });
+        }
+
+        return parseStringify({ accountId: null, error: "User not found" });
+    } catch (error) {
+        handleError(error, "Failed to sign in user");
+    }
 };
