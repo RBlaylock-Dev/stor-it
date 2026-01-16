6b30d4e40524c082ea363ac2bd066d0d684b6358
RBlaylock-Dev <rblaylock.prsvr@gmail.com>
2026-01-15 23:38:34 -0600

    implemented Sidebar Navigation, place for global search, place for file uploader, and MobileNavigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,25 @@
+import Header from '@/components/Header';
+import MobileNavigation from '@/components/MobileNavigation';
+import Sidebar from '@/components/Sidebar';
+import { getCurrentUser } from '@/lib/actions/user.actions';
 import React from 'react'
+import { redirect } from 'next/navigation';
 
-const Layout = ({ children }: { children: React.ReactNode}) => {
-  return <main className="flex h-screen">
-    Sidebar 
+const Layout = async ({ children }: { children: React.ReactNode}) => {
+    const currentUser = await getCurrentUser();
 
+    if(!currentUser) return redirect("/sign-in");
+  return (
+  <main className="flex h-screen">
+    <Sidebar {...currentUser} />
     <section className="flex h-full flex-1 flex-col">
-        MobileNavigation 
-        Header 
-
+        <MobileNavigation /> 
+        <Header />
         <div className="main-content">{children}</div>
     </section>
 
   </main>
+  )
 };
 
 export default Layout;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 // ** Create account flow**
 // 1. User enters full name and email
@@ -60,7 +61,7 @@ export const createAccount = async ({fullName, email, }: { fullName: string; ema
              {
                 fullName,
                 email,
-                avatar: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png",
+                avatar: avatarPlaceholderUrl,
                 accountId,
             },
         );
@@ -81,4 +82,21 @@ export const verifySecret = async ({ accountId, password }: {accountId: string; 
     } catch (error) {
         handleError(error, "Failed to verify OTP");
     }
-}
+};
+
+
+export const getCurrentUser = async () => {
+    const { databases, account } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersTableId,
+        [Query.equal("accountId", result.$id)],
+    );
+
+    if(user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
+};
