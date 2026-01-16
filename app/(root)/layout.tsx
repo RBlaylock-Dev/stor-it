5776cc523ccf369503a9961178e03888b8c4a1f8
RBlaylock-Dev <rblaylock.prsvr@gmail.com>
2026-01-16 15:55:03 -0600

    completed upload files functionality and implemented it.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,8 @@ import Sidebar from '@/components/Sidebar';
 import { getCurrentUser } from '@/lib/actions/user.actions';
 import React from 'react'
 import { redirect } from 'next/navigation';
+import { Toaster } from '@/components/ui/toaster';
+
 
 const Layout = async ({ children }: { children: React.ReactNode}) => {
     const currentUser = await getCurrentUser();
@@ -14,9 +16,11 @@ const Layout = async ({ children }: { children: React.ReactNode}) => {
     <Sidebar {...currentUser} />
     <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} /> 
-        <Header />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
     </section>
+
+    <Toaster />
 
   </main>
   )
